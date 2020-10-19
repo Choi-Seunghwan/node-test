65c4462fc594e9578cfb7912a3f5e080d683b5d2
Choi-Seunghwan <choiseunghwan.tech@gmail.com>
2020-10-19 16:43:51 +0900

    commit

--- test.js
+++ test.js
@@ -5,46 +5,53 @@ var SVGSpriter = require("svg-sprite"),
   File = require("vinyl"),
   glob = require("glob"),
   spriter = new SVGSpriter({
-    dest: "static/images/svg",
+    dest: "dest",
     mode: {
-      css: {
-        dest: "../",
-        bust: false,
-        render: {
-          scss: true,
-        },
-        prefix: "sprited-%s",
-        dimensions: true,
-      },
-      view: true,
-      // view: {
+      // css: {
+      //   dest: "./",
+      //   bust: false,
       //   render: {
       //     scss: true,
       //   },
+      //   prefix: ".img-%s",
+      //   dimensions: true,
       // },
+      view: {
+        dest: "./",
+        bust: false,
+        render: {
+          css: true,
+        },
+        prefix: ".img-%s",
+        dimensions: true,
+        layout: "vertical"
+      },
     },
   }),
-  cwd = path.resolve("svg");
+  cwd = path.resolve("./");
 
 // Find SVG files recursively via `glob`
-glob.glob("**/*.svg", { cwd: cwd }, function (err, files) {
+glob.glob("images/*.svg", function (err, files) {
   files.forEach(function (file) {
     // Create and add a vinyl file instance for each SVG
     spriter.add(
       new File({
         path: path.join(cwd, file), // Absolute path to the SVG file
-        base: "test", // Base path (see `name` argument)
+        base: path.resolve("images"), // Base path (see `name` argument)
         contents: fs.readFileSync(path.join(cwd, file)), // SVG file contents
       })
     );
   });
 
   spriter.compile(function (error, result, data) {
-    for (var type in result.css) {
+    for (const type in result.view) {
+      mkdirp.sync(path.dirname(result.view[type].path));
+      fs.writeFileSync(result.view[type].path, result.view[type].contents);
+    }
+
+    for (const type in result.css) {
       mkdirp.sync(path.dirname(result.css[type].path));
       fs.writeFileSync(result.css[type].path, result.css[type].contents);
     }
   });
 });
-
-console.log("completed");
